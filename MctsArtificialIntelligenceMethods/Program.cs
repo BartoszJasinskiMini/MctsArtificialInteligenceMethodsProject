c040e5624a5d4878b51282f8a9e4a084553fe1ca
Bartosz Jasinski <bjasinski@ittouch.pl>
2021-05-29 15:51:26 +0200

    Init commit

---
+++ MctsArtificialIntelligenceMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MctsArtificialIntelligenceMethods
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("asdf");
+        }
+    }
+}
